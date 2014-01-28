6f142203cb8121171323f47cabf55844e0333a65
BruceYang <an70711@gmail.com>
2014-01-28 20:11:31 +0800

    create node folder

---
+++ Node/input.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+</head>
+<body>
+	<form method="POST" action="output.php">
+		<h1 align="center"><font color="red">過年大樂透加碼100組</font></h1>
+		<table width="500" border="0" align="center">
+		<tr align="center">
+			<td width="100%" bgcolor="#EEEEEE">
+				<font color="red">注意!這只能對獎過年加碼100組!</font><br>
+
+				<font size="+1">輸入範例：<font color="#FF5511">010203040506<br>或(每個號碼之間家空格)01 02 03 04 05 06</font></font> <br>
+				選取期別<select name="select"><option>第102000014期百萬獎</option><option>第102000013期百萬獎</option></select><br>
+				請輸入對獎號碼:<input type="text" name="year" size="16"  ><br>
+				<input type="submit" value="對獎去"><br>
+				<font color="red">祝大家新年快樂!馬上中獎!^_^</font>
+			</td>
+			</tr>
+			</table>
+			<h2>公告</h2>
+		<table width="800" border="0">
+			<tr>
+				<td width="100%" bgcolor="#66FF66">
+					<font color="#0044BB">1.<font color="red">資料來源為台灣彩券</font>，公告獎號以台灣彩券為主，<font color="red">人有手殘，馬有腳軟!(中的話記得去台灣彩券再確定一次!)</font>本次查詢結果僅供參考。</font><br><br>
+				</td>
+			</tr>
+		</table>
+
+
+
+
+
+			<br><br><font color="blue">by Mark& BruceYang</font>
+
+</body>
+</html>
